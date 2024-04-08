cbdb1fcebd319ae9528e8de1dd05ef3a5a758781
Elias Heijari <69460338+eliazq@users.noreply.github.com>
2024-04-08 20:54:46 +0300

    Somewhat works, item pick up and drop
    With Client And Host

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,16 +7,18 @@ using UnityEngine.Events;
 
 [RequireComponent(typeof(NetworkObject))]
 [RequireComponent(typeof(FollowTransform))]
-public class Item : MonoBehaviour, IInteractable
+public class Item : NetworkBehaviour, IInteractable
 {
     public UnityEvent OnSoundMade;
     Rigidbody rb;
+    public FollowTransform followTransform { get; private set; }
 
     public bool isRigidbodyEnable { get { return !rb.isKinematic; } }
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        followTransform = GetComponent<FollowTransform>();
     }
 
     public string GetInteractText()
@@ -42,6 +44,11 @@ public class Item : MonoBehaviour, IInteractable
     public void Interact(Transform interactorTransform)
     {
         interactorTransform.GetComponent<ItemHandler>().SetItem(gameObject);
+    }
+
+    public NetworkObject GetNetworkObject()
+    {
+        return NetworkObject;
     }
 
     private void OnCollisionEnter(Collision collision)
--- Assets/Scripts/ItemHandler.cs
+++ Assets/Scripts/ItemHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Networking.Transport;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -8,7 +9,7 @@ public class ItemHandler : NetworkBehaviour
 {
     [SerializeField] private Transform hand;
     [SerializeField] private float throwForce = 300f;
-    private GameObject item;
+    private GameObject itemGameObject;
 
     private void Update()
     {
@@ -22,7 +23,7 @@ public class ItemHandler : NetworkBehaviour
 
     public void SetItem(GameObject itemGO)
     {
-        if (this.item == itemGO) return;
+        if (itemGameObject == itemGO) return;
 
         Item item = itemGO.GetComponent<Item>();
         if (item.isRigidbodyEnable)
@@ -30,7 +31,7 @@ public class ItemHandler : NetworkBehaviour
             item.DisableRigidbody();
         }
 
-        if (this.item == null)
+        if (itemGameObject == null)
         {
             SetItemToHand(itemGO);
         }
@@ -42,19 +43,43 @@ public class ItemHandler : NetworkBehaviour
         }
     }
 
-    private void SetItemToHand(GameObject item)
+    private void SetItemToHand(GameObject itemGO)
     {
-        item.GetComponent<FollowTransform>().target = hand;
-        this.item = item;
+        Item item = itemGO.GetComponent<Item>();
+        item.followTransform.target = hand;
+        itemGameObject = itemGO;
     }
 
     private void DropItem()
     {
-        Debug.Log("Drop Item");
-        if (item == null) return;
-        item.GetComponent<FollowTransform>().target = null;
-        item.GetComponent<Item>().EnableRigidbody();
-        item.GetComponent<Rigidbody>().AddForce(transform.forward * throwForce);
-        item = null;
+        if (itemGameObject == null) return;
+        Item item = itemGameObject.GetComponent<Item>();
+        DropItemServerRpc(item.GetNetworkObject());
+        itemGameObject = null;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void DropItemServerRpc(NetworkObjectReference networkObjectReference, ServerRpcParams serverRpcParams = default)
+    {
+        
+        networkObjectReference.TryGet(out NetworkObject itemNetworkObject);
+        DropItemClientRpc(itemNetworkObject);
+
+        Rigidbody itemRigidbody = itemNetworkObject.GetComponent<Rigidbody>();
+
+        NetworkObject senderNetworkObject = NetworkManager.Singleton.ConnectedClients[serverRpcParams.Receive.SenderClientId].PlayerObject;
+        Transform senderPlayerTransform = senderNetworkObject.transform;
+
+        itemRigidbody.AddForce(senderPlayerTransform.forward * throwForce);
+    }
+
+    [ClientRpc]
+    private void DropItemClientRpc(NetworkObjectReference networkObjectReference)
+    {
+        networkObjectReference.TryGet(out NetworkObject itemNetworkObject);
+        Item item = itemNetworkObject.GetComponent<Item>();
+
+        item.followTransform.target = null;
+        item.EnableRigidbody();
     }
 }
